ccecf6c297a9a9f6346c4b5474ab6866e4fd5c36
bulletz-break <91925965+bulletz-break@users.noreply.github.com>
2022-07-20 15:36:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -367,21 +367,21 @@ function washMach_json_mount() {
 
 // Função que envia o JSON para o ThingsBoard
 function washMach_json_send() {
-    let stepAttributes = [], entityId = {
-        entityType: "DEVICE",
-        id: "5e6e0110-d7b6-11ec-8715-a95ef7ab7919"
-    }, entityAttributeType = "SHARED_SCOPE";
+    // let stepAttributes = [], entityId = {
+    //     entityType: "DEVICE",
+    //     id: "5e6e0110-d7b6-11ec-8715-a95ef7ab7919"
+    // }, entityAttributeType = "SHARED_SCOPE";
 
-    stepAttributes.push({
-        key: "testeHoje",
-        value: washMach_json_final
-    });
+    // stepAttributes.push({
+    //     key: "testeHoje",
+    //     value: washMach_json_final
+    // });
 
-    self.ctx.attributeService.saveEntityAttributes(entityId, entityAttributeType, stepAttributes).subscribe(
-        function success() {
-            self.ctx.$scope.error = "";
-            self.ctx.detectChanges();
-        }); 
+    // self.ctx.attributeService.saveEntityAttributes(entityId, entityAttributeType, stepAttributes).subscribe(
+    //     function success() {
+    //         self.ctx.$scope.error = "";
+    //         self.ctx.detectChanges();
+    //     });
 }
 
 function washMach_form_show_saved_message() {
